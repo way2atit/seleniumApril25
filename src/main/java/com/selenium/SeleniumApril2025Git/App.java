402ed453b6ae9c18704e7e3d34f76b1549a2d213
way2atit <way2atit@gmail.com>
2025-07-09 11:59:12 +0530

    First COmmit

---
+++ src/main/java/com/selenium/SeleniumApril2025Git/App.java
@@ -0,0 +1,13 @@
+package com.selenium.SeleniumApril2025Git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/selenium/SeleniumApril2025Git/LoginTest.java
@@ -0,0 +1,9 @@
+package com.selenium.SeleniumApril2025Git;
+
+public class LoginTest {
+	
+	public static void main(String[] args) {
+		System.out.println("Login File -- Selnium April 2025 Git Topic");
+	}
+
+}
