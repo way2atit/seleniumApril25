f3dced212f4bd398bac64584f5eab32c95fc3ed6
way2atit <way2atit@gmail.com>
2025-07-09 12:18:43 +0530

    Change Admin File form lOcal

--- src/test/java/com/selenium/SeleniumApril2025Git/Admin.java
+++ src/test/java/com/selenium/SeleniumApril2025Git/Admin.java
@@ -1,5 +1,16 @@
 package com.selenium.SeleniumApril2025Git;
 
 public class Admin {
+	
+	
+	static int i;
+	static int j;
+	
+	public static void main(String[] args) {
+		
+		int sum = i +j;
+		
+	}
+	
 
 }
